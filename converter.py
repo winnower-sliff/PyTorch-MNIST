21ade643736e8eb93f12aedce46563dbe918c243
史蒂夫 <71089744+winnower-sliff@users.noreply.github.com>
2024-10-15 17:19:13 +0800

    10.15

--- converter.py
+++ converter.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@文件        :converter.py
+@说明        :用于将样本转为png图片
+@时间        :2024/10/15 17:17:04
+@作者        :winnower
+@版本        :1.0
+'''
+
 from utils import convert
 
 TRAIN_SET = './DataSets/MNIST/processed/training.pt'
